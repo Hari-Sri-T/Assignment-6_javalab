c7b2dc93fc1291a3c9040091c9260962f1c35c21
Hari-Sri-T <harisrishanmukhasai2005@gmail.com>
2025-02-19 12:00:35 +0530

    Created Main

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        RubberDuck rb = new RubberDuck();
+        System.out.println("--------------");
+        rb.display();
+        rb.performQuack();
+        rb.performSwim();
+        System.out.println("--------------");
+
+        RedheadDuck rh = new RedheadDuck();
+        System.out.println("--------------");
+        rh.display();
+        rh.performQuack();
+        rh.performSwim();
+        System.out.println("--------------");
+
+
+        MallardDuck md = new MallardDuck();
+        System.out.println("--------------");
+        md.display();
+        md.performQuack();
+        md.performSwim();
+        System.out.println("--------------");
+    }
+}
